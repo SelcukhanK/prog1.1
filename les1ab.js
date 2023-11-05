44e3b8aa0bc2eb7159f1c8e4bd42c06c1e6dd14e
SelcukhanK <56695065+SelcukhanK@users.noreply.github.com>
2023-11-05 15:53:00 +0100

    les 1 a b

---
+++ les1ab.js
@@ -0,0 +1,14 @@
+var name = "Erwin Henraat";
+console.log("Name: " + name);
+
+var age = 22;
+console.log("Age: " + age);
+
+var married = false;
+console.log("married? " + married);
+
+const readline = require('readline-sync');
+let answer = "";
+while (answer != "19"){
+    answer = readline.question("whats 9+10?");
+}
