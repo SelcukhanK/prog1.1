8db077a970b50a9a4fb8cd9643c9c356b762a912
SelcukhanK <56695065+SelcukhanK@users.noreply.github.com>
2023-11-05 15:46:56 +0100

    les 1

---
+++ les1.js
@@ -0,0 +1,14 @@
+// opdracht 1
+console.log("Hello world.");
+let getalX = 32;
+let getalY = 16;
+console.log(getalX * getalY);
+
+// opdracht 2
+
+let stringA = "MarioSpiderSonicMan";
+let sliced2 = stringA.slice(0,5);
+let sliced3 = stringA.slice(5,11);
+let sliced4 = stringA.slice(16,19);
+let sliced5 = stringA.slice(11,16);
+console.log(sliced2 + " " + sliced3 + sliced4 + " " + sliced5);
